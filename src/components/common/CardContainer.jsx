c4909a8c1b44aa458591b21e4f65cffebc42bab6
DarrenZLew <darrenzlew@gmail.com>
2019-09-25 23:30:51 -0500

    Update card container to center children

--- src/components/common/CardContainer.jsx
+++ src/components/common/CardContainer.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 
-const CardContainer = ({ children }) => {
+const useStyles = makeStyles(theme => ({
+  center: {
+    display: "flex",
+    justifyContent: "center"
+  }
+}));
+
+const CardContainer = ({ children, center }) => {
+  const classes = useStyles();
   return (
-    <Card>
+    <Card className={classes.center}>
       <CardContent>{children}</CardContent>
     </Card>
   );
