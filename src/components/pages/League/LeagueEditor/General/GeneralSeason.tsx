0d0c89bfda43fd8cfb01635d644eb853a0db1d4e
DarrenZLew <darrenzlew@gmail.com>
2019-10-09 00:27:17 -0500

    Export empty GeneralSeason component

---
+++ src/components/pages/League/LeagueEditor/General/GeneralSeason.tsx
@@ -0,0 +1,30 @@
+export {};
+
+// import React from "react";
+// import {
+//   Card,
+//   CardHeader,
+//   CardContent,
+//   Divider,
+//   Grid,
+//   CardActions,
+//   Button
+// } from "@material-ui/core";
+
+// export interface GeneralSeasonProps {}
+
+// const GeneralSeason: React.SFC<GeneralSeasonProps> = () => {
+//   return (
+//     <Card>
+//       <CardHeader title="Season" />
+//       <Divider />
+//       <CardActions>
+//         <Button color="primary" variant="contained">
+//           Start Season
+//         </Button>
+//       </CardActions>
+//     </Card>
+//   );
+// };
+
+// export default GeneralSeason;
